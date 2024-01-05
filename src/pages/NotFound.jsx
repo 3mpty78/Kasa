133e3cc0a358ccdc31848d113fa9f7766b3fc72a
3mpty <aurelien.corre@outlook.fr>
2024-01-05 19:25:40 +0100

    Created and designed 404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Appartement from "../components/Apartment";
+import Footer from "../components/Footer";
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
     return (
         <>
-            <Navbar />
-            <div>NotFound</div>
+            <main id="notFound">
+                <Navbar />
+                <div className="content">
+                    <h1>404</h1>
+                    <h2>Oups! La page que vous demandez n'existe pas.</h2>
+                    <Link to={"/"}>Retourner sur la page d'accueil</Link>
+                </div>
+            </main>
+            <Footer />
         </>
     );
 }
